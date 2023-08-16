cee60b5982e06b503049584e97ba284c3103f62b
SzymonSleboda <szymonskeboda@gmail.com>
2023-08-16 18:27:13 +0200

    files updated

---
+++ contacts.js
@@ -0,0 +1,26 @@
+const path = require('node:path');
+const fs = require("node:fs")
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+
+/*
+ * Skomentuj i zapisz wartość
+ * const contactsPath = ;
+ */
+
+// TODO: udokumentuj każdą funkcję
+function listContacts() {
+    // ...twój kod
+  }
+  
+  function getContactById(contactId) {
+    // ...twój kod
+  }
+  
+  function removeContact(contactId) {
+    // ...twój kod
+  }
+  
+  function addContact(name, email, phone) {
+    // ...twój kod
+  }
